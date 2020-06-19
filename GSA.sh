aab9ebfbc682dcdd4a3ae8d5dd78c136cddf69a1
laurafahey02 <43550524+laurafahey02@users.noreply.github.com>
2020-06-19 15:14:38 +0100

    Create GSA.sh

---
+++ GSA.sh
@@ -0,0 +1,23 @@
+## Step 1: Annotation ##
+
+# Map SNPs from GWAS results on to genes (GRCh37/hg19 start-stop coordinates +/-20kb).
+# Reference data files (g100_eur) and gene locations file were obtained from the MAGMA homepage
+
+magma --annotate window=20,20 --snp-loc g1000_eur.bim --gene-loc NCBI37.3.gene.loc --out ncbi37_eur
+
+## Step 2: Gene analysis ##
+
+#Compute gene P values for each GWAS dataset (SZ, ASD, IQ and EA).
+
+magma --bfile g1000_eur --pval [summary statistic file] N=[number of samples] --gene-annot ncbi37_eur.genes.annot --out [output_file_name] 
+
+## Step 3: Gene-set analysis ##
+
+# Test if the genes in each gene-set are more strongly associated with either phenotype than other genes in the genome.
+# First convert list of HGNC symbols to entrez IDs in the file format accepted by MAGMA by running hgnc2entrez.R. Combine multiple genesets together:
+
+awk 'FNR==1 && NR!=1 {print '\n'}{print}' geneset1.tab geneset2.tab > genesets.tab
+
+# Perform the gene-set analysis on all .raw files (output of step 2) for each geneset in genesets.tab 
+
+for i in *.raw; do id=$(echo ${i} | sed 's/.raw//'); magma --gene-results ${id}.raw --set-annot genesets.tab --out ${id}.results; done
